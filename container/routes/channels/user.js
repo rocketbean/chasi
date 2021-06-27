0dbc0053984c0c7eb5964f071ccc3f3f4d5697cc
nikko <buzzokkin@gmail.com>
2021-06-27 12:41:59 +0800

    upd

--- container/routes/channels/user.js
+++ container/routes/channels/user.js
@@ -4,7 +4,7 @@ module.exports = async function (socket) {
     socket.on("initiate", async data => {
         let session = await SessionStorage.fetch(await User.findById(data._id));
         if(session) {
-            session.initiateSocket(socket)
+            session.initiateSocket(socket) 
             socket.alias = data.email
         };
     });
